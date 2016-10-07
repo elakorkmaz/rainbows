ef3e9e7a04f51ecd47afb7d5d57950159070fa59
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-07 16:45:10 +0200

    lyrics in different colors

---
+++ example.js
@@ -0,0 +1,14 @@
+var colors = require('colors');
+
+function makeRainbow(string) {
+  console.log(colors.white(string));
+  console.log(colors.green(string));
+  console.log(colors.cyan(string));
+  console.log(colors.blue(string));
+  console.log(colors.red(string));
+  console.log(colors.magenta(string));
+}
+
+for (var i = 2; i < process.argv.length; i++) {
+  setInterval(makeRainbow, 300, process.argv[i]);
+}
