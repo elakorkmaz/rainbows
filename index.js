3d5ca75608740ecfa830fdece758303619ee29e5
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-07 14:38:21 +0200

    make rainbows

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var colors = require('colors');
 
-console.log('Hello World!'.rainbow);
+var myString = 'Hello World!';
+
+function makeRainbow() {
+  console.log(myString);
+  console.log(myString.yellow);
+  console.log(myString.green);
+  console.log(myString.cyan);
+  console.log(myString.blue);
+  console.log(myString.red);
+  console.log(myString.magenta);
+}
+
+setInterval(makeRainbow, 500);
